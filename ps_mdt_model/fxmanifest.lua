2c3304dece3ebb6cf4feb159a9b2db99853e4cdd
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2023-09-28 22:29:58 -0400

    added model

---
+++ ps_mdt_model/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game { 'gta5' }
+author 'Popcorn Roleplay - PrinceAlbert'
+description 'PS MDT Tablet Reskin'
+version '1.0'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/prop_cs_tablet_psmdt.ytyp'
+
